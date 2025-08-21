575a46b4dc09e6450df958708c5a99cdd15ca093
Onur Togan <36580970+otogan@users.noreply.github.com>
2025-08-21 14:21:30 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,81 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "tankP1Up":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . 5 . . . . . . . . 
+. . . . . . . 5 . . . . . . . . 
+. 5 e e . . . 5 . . . 5 e e . . 
+. 5 4 5 . . . 5 . . . e 4 4 . . 
+. e e 5 . 5 4 5 e e . e e e . . 
+. 5 4 5 5 5 4 4 4 4 e e 4 4 . . 
+. e e 5 5 4 5 5 4 4 4 e e e . . 
+. 5 4 5 5 4 5 4 e 4 4 e 4 4 . . 
+. e e 5 5 4 5 4 e 4 4 e e e . . 
+. 5 4 5 5 5 4 e e 4 4 e 4 4 . . 
+. e e 5 e 5 5 4 4 4 e e e e . . 
+. 5 4 5 . e e e e e . e 4 4 . . 
+. 4 e e . . . . . . . e e e . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image2":
+            case "myImage":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "tankP1MoveUp":
+            case "anim1":return [img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`];
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
---
+++ main.ts
@@ -0,0 +1,2 @@
+let mySprite = sprites.create(assets.image`tankP1Up`, SpriteKind.Player)
+controller.moveSprite(mySprite, 30, 30)
