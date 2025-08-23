edd24d2c5074365529da755d651885f89cd2387c
Onur Togan <36580970+otogan@users.noreply.github.com>
2025-08-23 10:57:37 -0400

    added p1 all direction images

--- main.ts
+++ main.ts
@@ -1,2 +1,15 @@
-let mySprite = sprites.create(assets.image`tankP1Up`, SpriteKind.Player)
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.setImage(assets.image`tankP1Up`)
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.setImage(assets.image`tankP1Left`)
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.setImage(assets.image`tankP1Right`)
+})
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.setImage(assets.image`tankP1Down`)
+})
+let mySprite: Sprite = null
+mySprite = sprites.create(assets.image`tankP1Up`, SpriteKind.Player)
 controller.moveSprite(mySprite, 30, 30)
